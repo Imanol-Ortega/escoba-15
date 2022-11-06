a86a679af4adc0e35c879bbfbe9d07ceb2ec16c0
Imanol-Ortega <imanicoortemohr018@gmail.com>
2022-11-06 19:38:07 -0300

    editando el index

---
+++ static/index.js
@@ -0,0 +1,4 @@
+/*let cartajugador = document.querySelectorAll('.cartajugador');
+*/
+let img = document.querySelector('.img-cartajugador');
+img.setAttribute("src",'static/cartasesp/reverso-min.png');
