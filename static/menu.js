fdb73c805050e867fe1a8c943a5845b571e4406e
Imanol-Ortega <imanicoortemohr018@gmail.com>
2022-12-15 14:59:55 -0300

    a

--- static/menu.js
+++ static/menu.js
@@ -2,3 +2,6 @@ document.querySelector('.elegir-modo').addEventListener('change',()=>{
     console.log(document.querySelector('.elegir-modo').value);
     document.querySelector('.dificultad').value = document.querySelector('.elegir-modo').value;
 });
+document.querySelector('.agg').addEventListener('click',()=>{
+    window.location.href = '/agregar.html';
+})
